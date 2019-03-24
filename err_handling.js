7780f79a894464db7719e09ee63f014e64c9cf14
yash1rj <yashrj130@gmail.com>
2019-03-24 20:37:19 +0530

    Create err_handling.js

---
+++ err_handling.js
@@ -0,0 +1,36 @@
+function validateName(name) {
+    try {
+        if(name.Match(/[\$\#]/)) {
+            console.log("Invalid input");
+            return false;
+        }
+        else {
+            console.log("Valid input");
+            return true;
+        }
+    }
+    catch (error) {
+        console.log("Error message: "+error.message);
+        if (error instanceof TypeError) {
+            console.log("Type Error occured");
+        }
+        else if (error instanceof SyntaxError) {
+            console.log("Syntax Error occured");
+        }
+        else if (error instanceof RangeError) {
+            console.log("Range Error occured");
+        }
+        else {
+            console.log("Some other Error occured");
+        }
+    }
+    finally {
+        console.log("Cleaning up resources");
+    }
+}
+
+function validate() {
+    validateName("Hello");
+}
+
+validate();
