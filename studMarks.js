8dddd15ac622cbcfdb72dee40c6d200f2c97e025
yash1rj <yashrj130@gmail.com>
2019-03-24 20:59:26 +0530

    Update studMarks.js

--- studMarks.js
+++ studMarks.js
@@ -1,31 +1,38 @@
-let studentMarks = [21,24,23,19,3,20,15,25,13,20];
+let studentMarks1 = [21,24,23,19,3,20,15,25,13,20];
+let studentMarks2 = [12,18,25,24,2,5,18,20,20,21];
 
 let avgMarks = 0;
 
 function find_more_than_avg(marks) {
-    for (let i=0; i<studentMarks.length; i++) {
-        avgMarks += studentMarks[i];
+    for (let i=0; i<marks.length; i++) {
+        avgMarks += marks[i];
     }
-    avgMarks /= studentMarks.length;
+    avgMarks /= marks.length;
     
     console.log("Average Marks of class: "+avgMarks);
     let sCount =0;
-    for (let i=0; i<studentMarks.length; i++) {
-        if (studentMarks[i] > avgMarks) {
+    for (let i=0; i<marks.length; i++) {
+        if (marks[i] > avgMarks) {
             sCount++;
         }
     }
-    let sGr8rAvg = (sCount/studentMarks.length)*100;
-    console.log("Percentage of students scoring more than avg: "+sGr8rAvg);
+    let sGr8rAvg = (sCount/marks.length)*100;
+    console.log("Percentage of students scoring more than avg: "+sGr8rAvg+"%");
 }
 
-function generate_frequency() {
+function generate_frequency(marks) {
+    let freqArr = [];
     for (let i=0; i<26; i++) {
-        if () {
-
+        let fCnt = 0;
+        for (let j=0; j<marks.length; j++) {
+            if (i === marks[j]) {
+                fCnt++;
+            }
         }
+        freqArr.push(fCnt);
     }
+    console.log("Frequency: ["+freqArr+"]");
 }
 
-find_more_than_avg();
-generate_frequency();
+find_more_than_avg(studentMarks2);
+generate_frequency(studentMarks2);
