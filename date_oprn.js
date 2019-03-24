2e3274f0e0bb25fcbcc8642e6bcfede1390d4a87
yash1rj <yashrj130@gmail.com>
2019-03-24 13:19:36 +0530

    Update date_oprn.js

--- date_oprn.js
+++ date_oprn.js
@@ -1,2 +1,10 @@
-var inDate = new Date("July 16, 2018 11:13:00");
-console.log(inDate.toDateString());
+let inDate = new Date("Feb 28, 2020 11:13:00");
+
+console.log("Start Date: "+inDate.toDateString().substring(4));
+
+let n = 1;
+n2millisec = n*24*60*60*1000;
+
+resDate = inDate.getTime() + n2millisec;
+resDate = new Date(resDate);
+console.log("Final Date: "+resDate.toDateString().substring(4));
