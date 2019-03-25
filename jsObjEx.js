64d42158b0827a58ece2529e689d49301d235dd4
yash1rj <yashrj130@gmail.com>
2019-03-26 00:00:37 +0530

    Update jsObjEx.js

--- jsObjEx.js
+++ jsObjEx.js
@@ -5,10 +5,14 @@ var student = {
 };
 
 let propVals ="";
+let cnt = 0;
 
 for(let property in student) {
+    cnt += 1;
     propVals += student[property];
-    propVals += ", ";
+    if (Object.keys(student).length != cnt ) {
+        propVals += ", ";
+    }
 }
 
 console.log(propVals);
