cf7d15823712587d4568609be226720148c4d655
yash1rj <yashrj130@gmail.com>
2019-03-27 00:00:24 +0530

    Update index.js

--- Calc/index.js
+++ Calc/index.js
@@ -1,3 +1,27 @@
 let opr1 = document.getElementById("opr1");
 let opr2 = document.getElementById("opr2");
 
+let addition = document.getElementById("plus");
+let subtraction = document.getElementById("minus");
+let multiplication = document.getElementById("multiply");
+let division = document.getElementById("divide");
+
+let btn = document.getElementById("calculate");
+
+let ans = document.getElementById("answer");
+
+btn.addEventListener('click', () => {
+    if (addition.checked) {
+        ans.innerHTML = Number(opr1.value)+Number(opr2.value);
+    }
+    else if (subtraction.checked) {
+        ans.innerHTML = Number(opr1.value)-Number(opr2.value);
+    }
+    else if (multiplication.checked) {
+        ans.innerHTML = Number(opr1.value)*Number(opr2.value);
+    }
+    else if (division.checked) {
+        ans.innerHTML = Number(opr1.value)/Number(opr2.value);
+    }
+})
+
