cf27036d95ae6c1b0e6702d9b0d8d76391719ab8
yash1rj <yashrj130@gmail.com>
2019-03-27 23:53:07 +0530

    Create promisEx2.js

---
+++ promisEx2.js
@@ -0,0 +1,30 @@
+function getTrip(location) {
+    return new Promise(function(resolve, reject) {
+        if (location == "ooty") {
+            resolve("Trip to "+location);
+        }
+        else {
+            reject(Error("Some error occured"));
+        }
+    });
+}
+
+// resolving
+getTrip("ooty").then(
+    (data) => {
+        console.log(data);
+    },
+    (error) => {
+        console.log(error);
+    }
+);
+
+// rejecting
+getTrip("coorg").then(
+    (data) => {
+        console.log(data);
+    },
+    (error) => {
+        console.log(error.message);
+    }
+);
