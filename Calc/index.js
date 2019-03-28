467672469e3284c0925d40cb1393555ad8690317
yash1rj <yashrj130@gmail.com>
2019-03-28 23:59:07 +0530

    Update index.js

--- Calc/index.js
+++ Calc/index.js
@@ -12,38 +12,49 @@ let ans = document.getElementById("answer");
 
 btn.addEventListener('click', () => {
     try {
-        if (!opr1.value.match(/^[0-9]+$/)) {
+        if (opr1.value == "" || opr2.value == "") {
+            throw new Error("Input all values");
+        }
+        else if (!opr1.value.match(/^[0-9]+$/)) {
             throw new Error("Operand 1 should be a numerical value");
         }
         else if (!opr2.value.match(/^[0-9]+$/)) {
             throw new Error("Operand 2 should be a numerical value");
+        }
+        else if (opr2.value == 0) {
+            throw new Error("Cannot divide by Zero");
         }
         else {
             if (addition.checked) {
                 ans.innerHTML = "Answer : ";
                 ans.innerHTML += (Number(opr1.value)+Number(opr2.value)).toFixed(3);
                 ans.style.backgroundColor = "green";
+                ans.style.textAlign ="center";
             }
             else if (subtraction.checked) {
                 ans.innerHTML = "Answer : ";
                 ans.innerHTML += (Number(opr1.value)-Number(opr2.value)).toFixed(3);
                 ans.style.backgroundColor = "green";
+                ans.style.textAlign ="center";
             }
             else if (multiplication.checked) {
                 ans.innerHTML = "Answer : ";
                 ans.innerHTML += (Number(opr1.value)*Number(opr2.value)).toFixed(3);
                 ans.style.backgroundColor = "green";
+                ans.style.textAlign ="center";
             }
             else if (division.checked) {
                 ans.innerHTML = "Answer : ";
                 ans.innerHTML += (Number(opr1.value)/Number(opr2.value)).toFixed(3);
                 ans.style.backgroundColor = "green";
+                ans.style.textAlign ="center";
             }
         }
     }
     catch(error) {
         ans.innerHTML = error.message;
         ans.style.backgroundColor = "red";
+        ans.style.textAlign ="center";
         
     }
 })
