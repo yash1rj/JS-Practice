d1ffe07042d766661924461a56d9d192b68b943b
yash1rj <yashrj130@gmail.com>
2019-03-28 23:40:26 +0530

    Update index.js

--- Calc/index.js
+++ Calc/index.js
@@ -11,6 +11,17 @@ let btn = document.getElementById("calculate");
 let ans = document.getElementById("answer");
 
 btn.addEventListener('click', () => {
+    try {
+        if ((/([0-9]+)/).test(opr1.value)) {
+            throw new Error("Operand 1 should be a numerical value");
+        }
+        else if ((/([0-9]+)/).test(opr2.value)) {
+            throw new Error("Operand 2 should be a numerical value");
+        }
+    }
+    catch(error) {
+        console.log(error.message);
+    }
     if (addition.checked) {
         ans.innerHTML = "Answer : ";
         ans.innerHTML += (Number(opr1.value)+Number(opr2.value)).toFixed(3);
