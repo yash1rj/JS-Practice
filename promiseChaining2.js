cebe508cf0efce445c4b0bb3ab798f03080ef55c
yash1rj <yashrj130@gmail.com>
2019-04-03 23:34:23 +0530

    Update promiseChaining2.js

--- promiseChaining2.js
+++ promiseChaining2.js
@@ -1,3 +1,28 @@
+// part1
+// fetch('./user.json')
+//     // load it as json
+//     .then(response => response.json())
+//     // make a request to github
+//     .then(user => fetch(`https://api.github.com/users/${user.name}`))
+//     // load the response as json
+//     .then(response => response.json())
+//     // Show the avatar image (githubUser.avatar_url) for 3 seconds
+//     .then(githubUser => {
+//         let img = document.createElement("img");
+//         img.src = githubUser.avatar_url;
+//         img.className = "promise-avatar-example"
+//         document.body.append(img);
+
+//         setTimeout(() => img.remove(), 3000);
+//     });
+// part1end
+
+    // how can we do something after the avatar has finished showing and gets removed? 
+    // For instance, we’d like to show a form for editing that user or something else. As of now, there’s no way.
+
+    // To make the chain extendable, we need to return a promise that resolves when the avatar finishes showing.
+
+// part2
 fetch('./user.json')
     // load it as json
     .then(response => response.json())
@@ -6,11 +31,25 @@ fetch('./user.json')
     // load the response as json
     .then(response => response.json())
     // Show the avatar image (githubUser.avatar_url) for 3 seconds
-    .then(githubUser => {
+    .then(githubUser => new Promise(function(resolve,reject) {
         let img = document.createElement("img");
         img.src = githubUser.avatar_url;
         img.className = "promise-avatar-example"
         document.body.append(img);
 
-        setTimeout(() => img.remove(), 3000);
-    });
+        setTimeout(() => {
+            img.remove();
+            resolve(githubUser);
+        }, 3000);
+    }))
+    // triggers after 3 seconds
+    .then(githubUser => alert(`Finished showing ${githubUser.name}`));
+
+// part2end
+
+// Now right after setTimeout runs img.remove(), it calls resolve(githubUser), 
+// thus passing the control to the next .then in the chain and passing forward the user data.
+
+// As a rule, an asynchronous action should always return a promise.
+
+// That makes it possible to plan actions after it. Even if we don’t plan to extend the chain now, we may need it later.
