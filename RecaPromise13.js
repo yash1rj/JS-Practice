840e053e774eba3d7b65a478bd5d07be73d52f33
yash1rj <yashrj130@gmail.com>
2019-04-06 19:15:30 +0530

    Create RecaPromise13.js

---
+++ RecaPromise13.js
@@ -0,0 +1,42 @@
+// Promise.race
+
+var promiseTRSANSG = (promiseThatResolvesAfterNSecondsGenerator = function(n=0) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve({
+                resolvedAfterNSeconds : n
+            });
+        }, n*1000);
+    });
+});
+
+var promiseTRJANSG = (promiseThatRejectsAfterNSecondsGenerator = function(n=0) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            reject({
+                rejectedAfterNSeconds : n
+            });
+        }, n*1000);
+    });
+});
+
+
+// case: one of the promises rejects first
+console.time("Promise.Race");
+var promisesArray = [];
+
+promisesArray.push(promiseTRSANSG(4));
+promisesArray.push(promiseTRSANSG(6));
+promisesArray.push(promiseTRSANSG(5));
+promisesArray.push(promiseTRJANSG(3));
+promisesArray.push(promiseTRSANSG(4));
+
+var handleAllPromises = Promise.race(promisesArray);
+handleAllPromises.then((values) => {
+    console.timeEnd("Promise.Race");
+    console.log("The fastest promise is resolved ", values);
+});
+handleAllPromises.catch((reason) => {
+    console.timeEnd("Promise.Race");
+    console.log("The fastest promise rejected with the following reason", reason);
+});
