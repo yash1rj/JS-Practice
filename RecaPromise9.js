ebb2aebbf88904a390a1b5036000a0270bd4c726
yash1rj <yashrj130@gmail.com>
2019-04-06 18:08:09 +0530

    Create RecaPromise9.js

---
+++ RecaPromise9.js
@@ -0,0 +1,39 @@
+// Promise.All
+
+var promiseTRSANSG = (promiseThatResolvesAfterNSecondsGenerator = function(n=0) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve({
+                resolvedAfterNSeconds : n
+            });
+        }, n*1000);
+    });
+});
+
+var promiseTRJANSG = (promiseThatRejectsAfterNSecondsGenerator = function(n=0) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            reject({
+                rejectedAfterNSeconds : n
+            });
+        }, n*1000);
+    });
+});
+
+
+// case: all promises are resovled
+console.time("Promise.All");
+var promisesArray = [];
+promisesArray.push(promiseTRSANSG(1));
+promisesArray.push(promiseTRSANSG(4));
+promisesArray.push(promiseTRSANSG(2));
+
+var handleAllPromises = Promise.all(promisesArray);
+handleAllPromises.then((values) => {
+    console.timeEnd("Promise.All");
+    console.log("All the promises are resolved ", values);
+});
+handleAllPromises.catch((reason) => {
+    console.timeEnd("Promise.All");
+    console.log("One of the promises failed with the following reason", reason);
+});
