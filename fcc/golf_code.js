51d8f5e080020aa7ab93d118578fa58b93985845
yash1rj <yashrj130@gmail.com>
2019-04-11 19:21:05 +0530

    Create golf_code.js

---
+++ fcc/golf_code.js
@@ -0,0 +1,25 @@
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+    if(strokes==1) {
+        return "Hole-in-one!";
+    }
+    else if(strokes <= par - 2) {
+        return "Eagle";
+    }
+    else if(strokes ==  par - 1) {
+        return "Birdie";
+    }
+    else if(strokes ==  par) {
+        return "Par";
+    }
+    else if(strokes ==  par + 1) {
+        return "Bogey";
+    }
+    else if(strokes == par + 2) {
+        return 	"Double Bogey";
+    }
+    return "Go Home!";
+}
+
+// Change these values to test
+console.log(golfScore(5, 4));
