abff20a2d4fbfb02bfbab5129d34ed31cc7296b8
yash1rj <yashrj130@gmail.com>
2019-04-13 17:16:24 +0530

    Create HOF.js

---
+++ fcc/HOF.js
@@ -0,0 +1,12 @@
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+
+const squareList = (arr) => {
+    "use strict";
+    // change code below this line
+    const squaredIntegers = arr.filter((num) => Number.isInteger(num) && num > 0).map((num) => Math.pow(num,2));
+    // change code above this line
+    return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
