9fd74d48ee36d90991cec60f217c0a250516fa89
yash1rj <yashrj130@gmail.com>
2019-04-13 11:26:46 +0530

    Update recordCollection.js

--- fcc/recordCollection.js
+++ fcc/recordCollection.js
@@ -1,27 +1,27 @@
 // Setup
 var collection = {
     "2548": {
-        "album": "Slippery When Wet",
-        "artist": "Bon Jovi",
-        "tracks": [ 
-            "Let It Rock", 
-            "You Give Love a Bad Name" 
-        ]
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [ 
+        "Let It Rock", 
+        "You Give Love a Bad Name" 
+      ]
     },
     "2468": {
-        "album": "1999",
-        "artist": "Prince",
-        "tracks": [ 
-            "1999", 
-            "Little Red Corvette" 
-        ]
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [ 
+        "1999", 
+        "Little Red Corvette" 
+      ]
     },
     "1245": {
-        "artist": "Robert Palmer",
-        "tracks": [ ]
+      "artist": "Robert Palmer",
+      "tracks": [ ]
     },
     "5439": {
-        "album": "ABBA Gold"
+      "album": "ABBA Gold"
     }
 };
 // Keep a copy of the collection for tests
@@ -29,20 +29,21 @@ var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 // Only change code below this line
 function updateRecords(id, prop, value) {
-    if (prop != "tracks" && value !="") {
-        collection[id].prop = value;
+  if (prop !== "tracks" && value !=="") {
+        collection[id][prop] = value;
     }
-    if (prop == "tracks" && collection[id].hasOwnProperty("tracks")) {
-        collection[id].prop = [];
-        if (value != "") {
-            collection[id].prop.push(value);
-        }
-    }
-    if (value == "") {
-        delete collection[id].prop;
-    }
+    if (prop === "tracks" && !collection[id].hasOwnProperty("tracks")) {
+        collection[id][prop] = [];
+      }
+    if (prop === "tracks" && value !== "") {
 
-    return collection;
+      collection[id][prop].push(value); 
+    }
+    
+    if(value =="") {
+            delete collection[id][prop];
+    }
+  return collection;
 }
 
 // Alter values below to test your code
