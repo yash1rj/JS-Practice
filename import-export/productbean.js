14db99a84a4c4ce136e5d458d6f96b6523a133ef
yash1rj <yashrj130@gmail.com>
2019-04-21 17:48:33 +0530

    Create productbean.js

---
+++ import-export/productbean.js
@@ -0,0 +1,8 @@
+export class Product {
+    constructor(prodId, productName, productQuantity, productPrice) {
+        this.prodId = prodId;
+        this.productName = productName;
+        this.productQuantity = productQuantity;
+        this.productPrice = productPrice;
+    }
+}
