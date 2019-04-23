8ecaef2622d41a9fd9dc2bfdc38e298bf3a678cd
yash1rj <yashrj130@gmail.com>
2019-04-23 23:39:03 +0530

    Update dynamicDropdown.js

--- AJAX-demo/DOM and AJAX/dynamicDropdown.js
+++ AJAX-demo/DOM and AJAX/dynamicDropdown.js
@@ -1,12 +1,19 @@
+let cityDrop = document.getElementById("city");
+
 function cityLister(state) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://127.0.0.1:5500/AJAX-demo/DOM%20and%20AJAX/state.json");
     xhr.onload = () => {
         obj = JSON.parse(xhr.responseText);
         var cityArr = obj[state];
-        console.log(cityArr);
+        // console.log(cityArr);
 
+        cityDrop.options.length = 0;
         for(var i=0; i < cityArr.length; i++) {
+            var opt = document.createElement('option');
+            opt.value = cityArr[i];
+            opt.innerHTML = cityArr[i];
+            cityDrop.appendChild(opt);
         }
     };
     xhr.send();
