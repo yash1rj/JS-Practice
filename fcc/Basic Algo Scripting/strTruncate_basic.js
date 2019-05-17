d4f1b33b7a193e17bb01bbefd74e2f4590ab1eff
Yash Raj <yashrj130@gmail.com>
2019-05-17 15:30:36 +0530

    Update strTruncate_basic.js

--- fcc/Basic Algo Scripting/strTruncate_basic.js
+++ fcc/Basic Algo Scripting/strTruncate_basic.js
@@ -11,3 +11,15 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+/*
+First we start off with a simple if statement to determine one of three outcomes…
+If our string length is greater than the num we want to truncate at, and our truncate 
+point is at least three characters or more into the string, we return a slice of our string starting at character 0, 
+  and ending at num - 3. We then append our '...' to the end of the string.
+However, if our string length is greater than the num but num is within the first three characters, we don’t have to 
+count our dots as characters. Therefore, we return the same string as above, with one difference: The endpoint of our 
+slice is now just num.
+Finally, if none of the above situations are true, it means our string length is less than our truncation num. 
+Therefore, we can just return the string.
+*/
