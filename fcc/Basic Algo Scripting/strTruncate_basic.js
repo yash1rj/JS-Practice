3897309295731ec89f028b48fc58ac2463c66d69
Yash Raj <yashrj130@gmail.com>
2019-05-17 15:29:44 +0530

    Create strTruncate_basic.js

---
+++ fcc/Basic Algo Scripting/strTruncate_basic.js
@@ -0,0 +1,13 @@
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if (str.length > num && num > 3) {
+    return str.slice(0, (num - 3)) + '...';
+  } else if (str.length > num && num <= 3) {
+    return str.slice(0, num) + '...';
+  } else {
+    return str;
+  }
+
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
