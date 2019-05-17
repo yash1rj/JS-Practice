a9f482f26d44abfe983663570bc6349cfac299f3
Yash Raj <yashrj130@gmail.com>
2019-05-17 15:37:38 +0530

    Create sliceSplice.js

---
+++ fcc/Basic Algo Scripting/sliceSplice.js
@@ -0,0 +1,21 @@
+/*
+You are given two arrays and an index.
+
+Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs.
+*/
+
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArray = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return localArray;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
