433a2e5c445473e6259c02d3727d610d4898286a
Yash Raj <yashrj130@gmail.com>
2019-05-22 16:18:47 +0530

    Create missingLetters_basic.js

---
+++ fcc/Intermediate Algo Scripting/missingLetters_basic.js
@@ -0,0 +1,28 @@
+function fearNotLetter(str) {
+
+  for(var i = 0; i < str.length; i++) {
+    /* code of current character */
+    var code = str.charCodeAt(i);
+
+    /* if code of current character is not equal to first character + no of iteration
+    hence character has been escaped */
+    if (code !== str.charCodeAt(0) + i) {
+
+      /* if current character has escaped one character find previous char and return */
+      return String.fromCharCode(code - 1);
+    }  
+  }
+  return undefined;
+}
+
+// test here
+fearNotLetter("abce");
+
+/*
+This solutions makes use of a for loop.
+Code of encountered character is stored in code.
+It is checked if code of current character is the expected one (no characters are skipped) by 
+using the logic - code of current character = code of first character + number of iterations.
+If a character is missing, the missing character is found and the final string is returned.
+undefined is returned if there is no missing character in the string.
+*/
