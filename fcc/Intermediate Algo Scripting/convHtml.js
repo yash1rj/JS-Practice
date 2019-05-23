8c849e3702b5237e62400f12aed0a20d2f358fff
Yash Raj <yashrj130@gmail.com>
2019-05-23 16:37:06 +0530

    Create convHtml.js

---
+++ fcc/Intermediate Algo Scripting/convHtml.js
@@ -0,0 +1,5 @@
+function convertHTML(str) {
+  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;');
+}
+
+console.log(convertHTML("Dolce & Gabbana"));
