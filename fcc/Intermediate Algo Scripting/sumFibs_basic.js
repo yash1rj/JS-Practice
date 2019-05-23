88f26a4e657780232c35457968eb879d5e388201
Yash Raj <yashrj130@gmail.com>
2019-05-23 16:41:07 +0530

    Create sumFibs_basic.js

---
+++ fcc/Intermediate Algo Scripting/sumFibs_basic.js
@@ -0,0 +1,26 @@
+function sumFibs(num) {
+    var prevNumber = 0;
+    var currNumber = 1;
+    var result = 0;
+    while (currNumber <= num) {
+        if (currNumber % 2 !== 0) {
+            result += currNumber;
+        }
+
+        currNumber += prevNumber;
+        prevNumber = currNumber - prevNumber;
+    }
+
+    return result;
+}
+
+// test here
+sumFibs(4);
+
+/*
+Create a variable to keep record of the current and previous numbers along with the result that will be returned.
+Use a while loop to make sure we do not go over the number given as parameter.
+We use the modulo operand to check if the current number is odd or even. If it is even, add it to the result.
+Complete the Fibonacci circle by rotating getting the next number and swapping values after.
+Return the result.
+*/
