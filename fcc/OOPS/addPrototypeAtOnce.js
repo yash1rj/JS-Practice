c2ebcb5957e905985a98e247091b73f87c89f74f
yash1rj <yashrj130@gmail.com>
2019-05-24 22:45:41 +0530

    Update addPrototypeAtOnce.js

--- fcc/OOPS/addPrototypeAtOnce.js
+++ fcc/OOPS/addPrototypeAtOnce.js
@@ -2,8 +2,12 @@ function Dog(name) {
     this.name = name; 
 }
 
+// There is one crucial side effect of manually setting the prototype to a new object. 
+// It erases the constructor property!
+
 Dog.prototype = {
     // Add your code below this line
+    constructor: Dog,   // define the constructor property
     numLegs: 2,
     eat: function(){
         console.log('nom nom nom');
@@ -16,3 +20,8 @@ Dog.prototype = {
 let beagle = new Dog("rocky");
 
 beagle.describe();
+
+console.log(beagle.constructor);
+// will print [Function: Object] if constructor is not defined on changing the prototype
+
+// When defined it should print [Function: Dog]
