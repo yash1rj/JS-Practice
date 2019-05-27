b718041d020b933ec2ea69fd5a69b8f419162571
yash1rj <yashrj130@gmail.com>
2019-05-28 01:16:55 +0530

    Create alphabeticSort.js

---
+++ fcc/Functional programming/alphabeticSort.js
@@ -0,0 +1,9 @@
+function alphabeticalOrder(arr) {
+  // Add your code below this line
+  return arr.sort(function(a,b) {
+    return a > b;
+  });
+  // Add your code above this line
+}
+
+console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
