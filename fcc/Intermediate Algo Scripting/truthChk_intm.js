9ad19245b696b565ab67091a2580af5c4c16f894
Yash Raj <yashrj130@gmail.com>
2019-05-30 17:06:25 +0530

    Create truthChk_intm.js

---
+++ fcc/Intermediate Algo Scripting/truthChk_intm.js
@@ -0,0 +1,12 @@
+function truthCheck(collection, pre) {
+  return collection.every(function (element) {
+    return element.hasOwnProperty(pre) && Boolean(element[pre]);
+  });
+}
+
+// test here
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
+/*
+Uses the native “every” method to test whether all elements in the array pass the test.
+*/
